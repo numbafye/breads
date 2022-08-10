b456084e07baa5d4042eae50bf434dbc7bc1b550
unknown <saadiqgp@gmail.com>
2022-08-10 19:14:15 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 // DEPENDENCIES
-const { application } = require("express");
 const express = require("express");
 
-// CONFIGURATION (between .env and .js "node server.js")
+// CONFIGURATION
 require("dotenv").config();
 const PORT = process.env.PORT;
-console.log(PORT);
+const app = express();
 
 // ROUTES
-application.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to an Awesome App about Breads!");
+});
+
+// Breads
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
 
 // LISTEN
-application.listen(PORT, () => {
-    console.log('listening on port', PORT);
-})
+app.listen(PORT, () => {
+  console.log("listening on port", PORT);
+});
+
